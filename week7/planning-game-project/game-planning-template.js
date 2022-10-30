0dca3cab377ccaa7e71cc0393d9e6ddc695b2518
katherine rodriguez <katherine.rodriguezz.ny@gmail.com>
2022-10-30 17:27:38 -0400

    First commit

---
+++ week7/planning-game-project/game-planning-template.js
@@ -0,0 +1,69 @@
+//Game title: Tic Tac Toe or like a wordle game; Divs are good for tic tac toe
+
+ /*General Overview of Gameplay 
+Describe the sequence of events that happen in your game
+include how the score is used 
+include how the game will end - including win/lose message 
+
+1. This Tic Tac Toe game will be played by the player and the computer.
+2.There will be an option to choose if the player wants to be "X" or "O", might decide to pick colors instead of traditional "X", "O"
+3. There will be 9 boxes
+4. When a player wins, there will be a pop up that says "YOU WON!"  Maybe a little animation
+5. Maybe keep score of how many times the player/computer has won, 2/3
+*/
+
+
+
+/*Requirements - include a short note about how your game will fulfill each of these requirements 
+
+user interaction causes changes to the web page using DOM methods (at least 3 changes during game play) can be images, text, score indicator 
+  - In Tic Tac Toe, clicking the square will cause an "X" or an "O" to appear.
+  -Score tab
+  -Win or Lose
+
+correct use of at least 1 JavaScript Class that is used in the game 
+  -Define variables like the name of the player using "let or Const"
+  -A constructor for things that both players will have in common like the score, name, action
+
+use of a method defined in your class 
+  -randomChoice use for a spot on the tic tac toe board
+  -addToScore for accumulation of score
+
+keep score - user must be able to gain points
+  -The addToScore should facilitate this
+
+declare if the user won or lost at the end of the game and display on the page 
+  -Will use an alert that suggests the winner 
+
+ Use of CSS to set colors, fonts and overall layout of the page
+  -will use divs and grids to create the boxes for tik tac toe
+  -will consider using colors instead of "X" and "O" for the box fillers
+-Flexbox
+
+use of iteration. examples: for loop, while loop, .forEach method, .map method
+-Not sure of what iteration I will be using if any
+  
+  
+*/
+
+
+
+/* Plan functions 
+start thinking about your basic functions! give them a name and list what they do 
+-chooseBox or maybe chooseColor if the boxes are different colors
+-chooseCharacter "smiley face" "maybe an emoji?"
+-console.log for outputs
+
+playGame for amount of times the game is played.
+
+
+
+
+
+
+
+*/
+
+
+
+
